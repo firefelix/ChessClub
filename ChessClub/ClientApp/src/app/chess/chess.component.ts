cac661b421e2f9bc52028dbf690a11bc43353170
Dylan <dsouvage@gmail.com>
2019-07-28 15:41:57 -0400

    creating test homepage (chess), added google maps however functionality not working

---
+++ ChessClub/ClientApp/src/app/chess/chess.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chess',
+  templateUrl: './chess.component.html'
+})
+export class ChessComponent {
+  chess = {
+    title: 'Home',
+    subtitle: 'Welcome Home!',
+    content: 'Some home content.',
+    image: 'assets/smiles.jpg'
+  }
+}
