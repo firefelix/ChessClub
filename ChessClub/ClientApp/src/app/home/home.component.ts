d5b1bb009a43e7ae605c256cd7fc3c9eb3f317d5
Dylan <dsouvage@gmail.com>
2019-07-28 16:48:25 -0400

    removing some stuff off navbar, moving chess to home

--- ChessClub/ClientApp/src/app/home/home.component.ts
+++ ChessClub/ClientApp/src/app/home/home.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  home = {
+    title: 'Home',
+    subtitle: 'Welcome Home!',
+    content: 'Some home content.',
+    image: 'assets/smiles.png'
+  }
 }
